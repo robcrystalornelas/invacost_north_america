72cec6571f2b243584351bf55366b399514ced97
Robert Crystal-Ornelas <rcrystalornelas@usfca.edu>
2020-08-13 17:55:39 -0700

    new barplot and new data aggregation code for environment.

--- scripts/economic_impact_by_ecosystem.R
+++ scripts/economic_impact_by_ecosystem.R
@@ -7,23 +7,24 @@ source("/Users/robcrystalornelas/Desktop/research/INVACOST_NorthAmerica/invacost
 # Here are the data that we need: both highly reliable and directly observed measurements
 head(expanded_observed_and_high_and_country)
 
-levels(expanded_observed_and_high_and_country$Environment)
+unique(expanded_observed_and_high_and_country$Environment)
 
-class(expanded_observed_and_high_and_country$cost_bil)
-grouped_environment <-
-  expanded_observed_and_high_and_country %>% group_by(Environment) %>%
-  summarise(cost_by_environment = sum(numeric(cost_bil)))
+# Make a table of the  costs grouped by environment
+aggregated_environment_costs <- aggregate(cost_bil~Environment,data=expanded_observed_and_high_and_country,FUN="sum")
+aggregated_environment_costs
 
-gg <-
-  ggplot(grouped_environment,
-         aes(x = Environment, y = cost_by_environment))
-gg <- gg + geom_bar(stat = "identity", fill = "darkgreen")
-gg <- gg + theme_cowplot()
-gg <- gg + ylab("Cost (US$ billions)")
-gg <- gg + xlab("Environment")
-gg
-gg <-
-  gg + theme(
+enviro_costs_plot <-
+  ggplot(aggregated_environment_costs,
+         aes(x = reorder(Environment, -cost_bil), y = cost_bil))
+enviro_costs_plot <-
+  enviro_costs_plot + geom_bar(stat = "identity", fill = "#29AF7FFF")
+enviro_costs_plot <- enviro_costs_plot + theme_cowplot()
+enviro_costs_plot <-
+  enviro_costs_plot + ylab("US$ Billions (2017 value)")
+enviro_costs_plot <- enviro_costs_plot + xlab("Environment")
+enviro_costs_plot
+enviro_costs_plot <-
+  enviro_costs_plot + theme(
     axis.text = element_text(size = 25),
     # Change tick mark label size
     axis.title = element_text(size = 25, face = "bold"),
@@ -33,11 +34,12 @@ gg <-
       vjust = 0.5
     ),
     strip.text = element_text(size = 25)
-  ) # Change axis title size
+  )
+enviro_costs_plot <-
+  enviro_costs_plot + scale_y_continuous(expand = c(0, 0)) # This text stops bars from floating above x-axis
+enviro_costs_plot
 
-gg
 
- 
 # Some other ways of identifying costs
 aggregate(cost_bil~Environment,data=expanded_observed_and_high,FUN="sum")
 aggregate(cost_bil~Environment,data=expanded_observed_and_high,FUN="length")
