d74f73839c7dc3157464fa8a467313a0bf6a4db5
Robert Crystal-Ornelas <rcrystalornelas@usfca.edu>
2020-08-17 12:20:01 -0700

    renaming files, so they are a bit more uniform

--- scripts/economic_impact_by_country.R
+++ scripts/economic_impact_by_country.R
@@ -6,7 +6,8 @@ source("scripts/filtering_and_cleaning_data.R")
 
 # read in research effort data
 research_effort <- read.csv("data/research_effort_north_america.csv", head = TRUE)
-research_effort <- research_effort[,c(1,4)]
+names(research_effort)
+research_effort <- research_effort[,c(1,6)]
 research_effort
 # we are going to use the final column "research effort as expenditure in US$"
 
@@ -25,7 +26,8 @@ joined_country_and_research_effort <- left_join(aggregated_country_costs, resear
 head(joined_country_and_research_effort)
 
 # Now, use mutate to scale costs in each country by research effort in each country
-costs_scaled_by_research_effort <- mutate(joined_country_and_research_effort, costs_scaled_by_effort = cost_bil / research_effort_as_expenditure_in_R_and_D_in_percent_GDP)
+head(research_effort)
+costs_scaled_by_research_effort <- mutate(joined_country_and_research_effort, costs_scaled_by_effort = cost_bil / Research_effort_as_expenditure_in_R_and_D_in_current._USD)
 costs_scaled_by_research_effort
 
 country_costs_plot <-
@@ -35,7 +37,7 @@ country_costs_plot <-
   country_costs_plot + geom_bar(stat = "identity", fill = "#453781FF")
 country_costs_plot <- country_costs_plot + theme_cowplot()
 country_costs_plot <-
-  country_costs_plot + ylab("US$ Billions (2017 value)")
+  country_costs_plot + ylab("Economic impact scaled by science research funding")
 country_costs_plot <- country_costs_plot + xlab("Country")
 country_costs_plot
 country_costs_plot <-
--- scripts/economic_impact_by_taxa.R
+++ scripts/economic_impact_by_taxa.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(invacost)
 
-source("/Users/robcrystalornelas/Desktop/research/INVACOST_NorthAmerica/invacost_north_america/scripts/filtering_and_cleaning_data.R")
+source("scripts/filtering_and_cleaning_data.R")
 
 # See all the different levels of phyla within the database
 unique(expanded_observed_and_high_and_country$Phylum)
@@ -24,6 +24,7 @@ inverts <-
     "Nematoda/unknown"
   ),]
 sum(inverts$cost_bil)
+dim(inverts)
 
 # subset plants
 plants <-
@@ -32,13 +33,13 @@ plants <-
                                                                       "Haptophyta",
                                                                       "Tracheophyta/Unspecified"),]
 sum(plants$cost_bil)
-
+dim(plants)
 # Subset verts
 verts <-
   expanded_observed_and_high_and_country[expanded_observed_and_high_and_country$Phylum %in% c("Chordata",
                                                                       "Chordata/Diverse"),]
 sum(verts$cost_bil)
-
+dim(verts)
 # Anything else not captured by above categories
 other <-
   expanded_observed_and_high_and_country[expanded_observed_and_high_and_country$Phylum %in% c(
@@ -63,7 +64,7 @@ other <-
     "Picornavirales"
   ),]
 sum(other$cost_bil)
-
+dim(other)
 # More minor taxonomic groupings
 cost_by_fine_taxonomic_groups <- aggregate(expanded_observed_and_high_and_country$cost_bil, by=list(Category=expanded_observed_and_high_and_country$Phylum), FUN=sum)
 cost_by_fine_taxonomic_groups[order(cost_by_fine_taxonomic_groups$x),]
